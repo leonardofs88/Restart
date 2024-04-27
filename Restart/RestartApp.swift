158c074d1400a5b5d03cf4183ec9289618716aad
Leonardo Soares <leonardofs88@live.com>
2024-04-27 16:33:19 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Leonardo Soares on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
